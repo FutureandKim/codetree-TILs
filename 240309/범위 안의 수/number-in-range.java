2af02c480829f0cbb9067cf1e81091158d3600b3
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-09 16:48:15 +0900

    Title: 범위 안의 수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240309/범위 안의 수/number-in-range.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = sc.nextInt();
+        int a = sc.nextInt();
+
+        System.out.print(a >= 10 && a <= 20 ? "yes" : "no");
+    }
+}
