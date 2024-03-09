397b31aeb7f63d357d2bc897cebeddd68ac38021
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-10 03:13:17 +0900

    Title: a/b 출력, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240310/a/b 출력/a-divide-b.java
+++ 240310/a/b 출력/a-divide-b.java
@@ -7,6 +7,12 @@ public class Main {
         int b = sc.nextInt();
 
         double res = (double) a / (double) b;
-        System.out.printf("%.20f", res);
+        
+        System.out.print("0.");
+        for(int i = 0; i < 20; i++){
+            System.out.print((a*10)/b);
+            a = (a*10)%b;
+        }
+            
     }
 }
