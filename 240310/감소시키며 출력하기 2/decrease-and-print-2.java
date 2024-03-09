94711dd70f9af2f149c8573eda0320ba2d09a6e7
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-10 02:49:06 +0900

    Title: 감소시키며 출력하기 2, Time: 100ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240310/감소시키며 출력하기 2/decrease-and-print-2.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 26;
+
+        while(n >= 10){
+            System.out.printf("%d ", n);
+            n--;
+        }       
+    }
+}
