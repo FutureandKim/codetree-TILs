342dc053c4c25f7c7b03f8f34d15a1e15f34f99f
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-09 18:01:55 +0900

    Title: 남녀노소 구분짓기, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 240309/남녀노소 구분짓기/sex-and-age.java
+++ 240309/남녀노소 구분짓기/sex-and-age.java
@@ -7,13 +7,13 @@ public class Main {
         int age = sc.nextInt();
         
         if(gen == 0){
-            if(age >= 20)
+            if(age >= 19)
                 System.out.print("MAN");
             else
                 System.out.print("BOY");
         }
         else{
-            if(age >= 20)
+            if(age >= 19)
                 System.out.print("WOMAN");
             else  
                 System.out.print("GIRL");
