c5811feff176ac1554866ffdbd8ce93be5fb2f7e
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-10 14:52:07 +0900

    Title: a 부터 b 까지, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240310/a 부터 b 까지/a-to-b.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int n = a;
+
+        System.out.print(a + " ");
+        while(n < b){
+            if(n % 2 == 1)
+                n *= 2;
+            else
+                n += 3;
+            System.out.printf("%d ", n);
+
+        }
+    }
+}
