7e3ca7b6a57d8b36b07ec95bdb3ca7bc65c0f787
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-10 15:03:34 +0900

    Title: 369 게임, Time: 132ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240310/369 게임/369-game.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++){
+            if(i < 10){
+                if(i % 3 == 0)
+                    System.out.printf("%d ", 0);
+                else
+                    System.out.printf("%d ", i);
+            } else{
+                if((i / 10) % 3 == 0 || (i % 10) % 3 == 0)
+                    System.out.printf("%d ", 0);
+                else
+                    System.out.printf("%d ", i);
+            }
+        }
+    }
+}
