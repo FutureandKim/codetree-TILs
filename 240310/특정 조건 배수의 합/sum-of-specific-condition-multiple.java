2a5bf46d7153434c69df7dfe6fd1c74b3be2f1f9
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-10 18:06:00 +0900

    Title: 특정 조건 배수의 합, Time: 162ms, Memory: 10MB, Status: Passed - CodeTree

--- 240310/특정 조건 배수의 합/sum-of-specific-condition-multiple.java
+++ 240310/특정 조건 배수의 합/sum-of-specific-condition-multiple.java
@@ -8,11 +8,11 @@ public class Main {
         int sum = 0;
 
         if (a >= b){
-            int temp = b;
+            int temp = a;
             a = b;
             b = temp;
         }
-        
+
         for(int i = a; i <= b; i++){
             if(i % 5 == 0)
                 sum += i;
