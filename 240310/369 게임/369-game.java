bb29a4e3b5207b793d493fb372639de5851e7f49
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-10 15:48:24 +0900

    Title: 369 게임, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

--- 240310/369 게임/369-game.java
+++ 240310/369 게임/369-game.java
@@ -6,16 +6,17 @@ public class Main {
         int n = sc.nextInt();
 
         for(int i = 1; i <= n; i++){
-            if(i % 3 == 0){
-                System.out.printf("%d ", 0);
-            }else if(i <= 10){
+            if(i % 3 == 0) // 3의 배수인 경우 
+                System.out.printf("%d ", 0); 
+            else if(i <= 10) // 3의 배수가 아닌 10 이하의 자연수
                 System.out.printf("%d ", i);
-            }else if(i % 10 == 0){
+            else if(i % 10 == 0){ // 3의 배수가 아닌 10 초과 수 중 10의 배수
                 if((i/10)%3 == 0)
                     System.out.printf("%d ", 0);
                 else
                     System.out.printf("%d ", i); 
-            }else {
+            }
+            else { // 나머지 수
                 if((i / 10) % 3 == 0 || (i % 10) % 3 == 0)
                     System.out.printf("%d ", 0);
                 else
