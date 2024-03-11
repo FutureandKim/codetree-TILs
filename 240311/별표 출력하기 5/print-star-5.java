08f8fba30aef1ff07145df2c24eebcb19f000b85
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-11 16:15:52 +0900

    Title: 별표 출력하기 5, Time: 164ms, Memory: 10MB, Status: Passed - CodeTree

--- 240311/별표 출력하기 5/print-star-5.java
+++ 240311/별표 출력하기 5/print-star-5.java
@@ -5,9 +5,9 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n-i; j++){
-                for(int z = 1; z < n-i; z++)
+        for(int i = 0; i < n; i++){ //몇 줄 출력할 것인가
+            for(int j = 0; j < n-i; j++){ //가로로 몇 세트 출력할 것인가
+                for(int z = 1; z < n-i; z++) //한 세트에 몇 개의 *을 출력할 것인가
                     System.out.print("*");
                 System.out.print("* ");
             }
