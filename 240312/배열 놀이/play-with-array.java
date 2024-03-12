1d87e8a6a12fd18fba5b04f4ed81acc2748967e8
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-12 23:49:43 +0900

    Title: 배열 놀이, Time: 205ms, Memory: 11MB, Status: Wrong Answer - CodeTree

---
+++ 240312/배열 놀이/play-with-array.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int q = sc.nextInt();
+        int[] arr = new int[n];
+
+        // 원소 입력
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        for(int i = 0; i < q; i++){
+            int qnum = sc.nextInt(); // 질의 번호
+            int a = 0, b = 0;
+            if(qnum == 1){
+                a = sc.nextInt();
+                System.out.println(arr[a-1]);
+            } else if(qnum == 2){
+                a = sc.nextInt();
+                for(int j = 0; j < n; j++){
+                    if(arr[j] == a){
+                        System.out.println(j+1);
+                        break;
+                    }
+                }
+            } else if(qnum == 3){
+                a = sc.nextInt();
+                b = sc.nextInt();
+                for(int j = a-1; j < b; j++)
+                    System.out.printf("%d ", arr[j]);
+            }
+        }
+
+        sc.close();
+    }
+}
