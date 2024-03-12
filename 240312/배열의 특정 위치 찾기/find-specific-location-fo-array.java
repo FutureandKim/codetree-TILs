80472f656128da43b7235d3c5a59fc46b8d96ccf
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-12 18:30:24 +0900

    Title: 배열의 특정 위치 찾기, Time: 152ms, Memory: 10MB, Status: Passed - CodeTree

--- 240312/배열의 특정 위치 찾기/find-specific-location-fo-array.java
+++ 240312/배열의 특정 위치 찾기/find-specific-location-fo-array.java
@@ -4,15 +4,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] a = new int[10];
-        int sum = 0, cnt = 0;
+        int sum_2 = 0, sum_3 = 0, cnt_3 = 0;
 
         for(int i = 0; i < 10; i++){
             a[i] = sc.nextInt();
-            if(i % 2 != 0){
-                sum += a[i];
-                cnt++;
+            if((i+1) % 2 == 0)
+                sum_2 += a[i];
+            if((i+1) % 3 == 0){
+                sum_3 += a[i];
+                cnt_3++;
             }
         }
-        System.out.printf("%d %.1f", sum, (double)sum/cnt);
+        System.out.printf("%d %.1f", sum_2, (double)sum_3/cnt_3);
     }
 }
