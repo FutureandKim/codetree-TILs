e37f763515c89fc2f2c558e878d115f9f1dbc1ea
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-13 00:22:16 +0900

    Title: 연속부분수열일까, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240313/연속부분수열일까/contiguous-array-or-not.java
+++ 240313/연속부분수열일까/contiguous-array-or-not.java
@@ -7,7 +7,7 @@ public class Main {
         int n2 = sc.nextInt();
         int[] a = new int[n1];
         int[] b = new int[n2];
-        int idx = -1;
+        int idx_a = -1, idx_b = 0;
         boolean res = true;
 
         for(int i = 0; i < n1; i++)
@@ -20,25 +20,27 @@ public class Main {
         // b[0]이 배열 a의 몇번째 인덱스에 위치하는지 탐색
         for(int i = 0; i < n1; i++){
             if(a[i] == b[0]){
-                idx = i;
+                idx_a = i;
                 break;
             }
         }
 
         // 부분 수열이 될 수 없는 경우 우선 제외
-        if(idx == -1 || (idx+n2) > n1){
+        if(idx_a == -1 || (idx_a+n2) > n1){
             System.out.printf("No");
             res = false;
         }
 
         if(res == true) {
-            for(int j = idx; j < (idx+n2); j++){
-                //System.out.printf("%d %d\n", j, (j-(idx)));
-                if(a[j] != b[j-idx]){
+            for(int j = idx_a; j < (idx_a+n2); j++){
+                //System.out.printf("%d %d\n", j, idx_b);
+                
+                if(a[j] != b[idx_b]){
                     System.out.print("No");
                     res = false;
                     break;
                 }
+                idx_b++;
             }
         }
         if(res == true)
