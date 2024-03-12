1f25e790f89b7ec8520454d170fbff758cf4ee1c
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-12 23:38:33 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

--- 240312/2가 3번째로 등장하는 위치/where-2-appears-3rd.java
+++ 240312/2가 3번째로 등장하는 위치/where-2-appears-3rd.java
@@ -5,14 +5,14 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
-        int cnt = 0, arr_num; // 2의 개수 카운트, 3번째 2 위치 
+        int cnt = 0, arr_num = 0; // 2의 개수 카운트, 3번째 2 위치 
 
         for(int i = 0; i < n; i++){
             arr[i] = sc.nextInt();
             if(arr[i] == 2){
                 cnt++;
                 if(cnt == 3)
-                    arr_num = i;
+                    arr_num = (i+1);
             }
         }
         System.out.print(arr_num);
