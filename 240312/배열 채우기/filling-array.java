eeaa67a40b119056136cbd1a05eae6e3e1ed1055
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-12 16:50:11 +0900

    Title: 배열 채우기, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240312/배열 채우기/filling-array.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+
+        for(int i = 0; i < 10; i++){
+            arr[i] = sc.nextInt();
+            if(arr[i] == 0){
+                for(int j = 0; j <= i-1; j++)
+                    System.out.printf("%d ", arr[(i-1)-j]);
+                break;
+            }
+        }
+    }
+}
