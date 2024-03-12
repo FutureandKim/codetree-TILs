caba906cdf35d9fdddaa84d0ffd3de847113f92a
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-12 21:00:14 +0900

    Title: 숫자들의 배수, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240312/숫자들의 배수/multiple-of-numbers.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[10];
+        int i, cnt = 0;
+
+        for(i = 0; i < 10; i++){
+            a[i] = n * (i+1);
+            if(a[i] % 5 == 0){
+                if(cnt == 2)
+                    break;
+                cnt++;
+            }
+        }
+
+        for(int j = 0; j < i; j++)
+            System.out.printf("%d ", a[j]);
+
+
+    }
+}
