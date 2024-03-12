1441b9981d035de9e0b5cbf37e4fc4651c34ebab
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-12 18:35:49 +0900

    Title: 배열의 특정 위치 찾기 3, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

--- 240312/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.java
+++ 240312/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.java
@@ -8,7 +8,7 @@ public class Main {
         for(int i = 0; i < 100; i++){
             a[i] = sc.nextInt();
             if(a[i] == 0){
-                System.out.printf("%d", a[i-3]+a[i-2]+a[i-1]);
+                System.out.print(a[i-3]+a[i-2]+a[i-1]);
                 break;
             }
         }
