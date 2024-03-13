4f708f420ab5a064e7226ad433065fc146f11b75
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-13 20:19:38 +0900

    Title: 대문자로 바꾸기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240313/대문자로 바꾸기/change-to-capital.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char[][] arr = new char[5][3];
+
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j < 3; j++){
+                char c = sc.next().charAt(0);
+                System.out.printf("%c ", (int)c-32);
+            }
+            System.out.println();
+            sc.close();
+        }
+    }
+}
