ba9629542126162079e8732c52f2f53fe2145dc3
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-13 22:18:06 +0900

    Title: 아래로 사각형 채우기, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240313/아래로 사각형 채우기/filling-rectangle-with-downflow.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n][n];
+        int cnt = 1;
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++)
+                arr[j][i] = cnt++;
+        }
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++)
+                System.out.printf("%d ", arr[i][j]);
+            System.out.println();
+        }
+
+
+    }
+}
