4a02b56a731520c83872f1f3bc798ed7a5393c86
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-13 23:17:31 +0900

    Title: 대각선으로 숫자 채우기, Time: 574ms, Memory: 21MB, Status: Passed - CodeTree

--- 240313/대각선으로 숫자 채우기/diagonal-numbering.java
+++ 240313/대각선으로 숫자 채우기/diagonal-numbering.java
@@ -6,8 +6,9 @@ public class Main {
         int n = sc.nextInt();
         int m = sc.nextInt();
         int[][] a = new int[n][m];
-        int cnt = 1, num = 0;
+        int cnt = 1;
 
+        // 대각선마다 (행 idx) + (열 idx) 값이 같음을 전제
         for(int k = 0; k <= m+n; k++){
             for(int i = 0; i < n; i++){
                 for(int j = 0; j < m; j++){
@@ -22,5 +23,7 @@ public class Main {
                 System.out.printf("%d ", a[i][j]);
             System.out.println();
         }
+
+        sc.close();
     }
 }
