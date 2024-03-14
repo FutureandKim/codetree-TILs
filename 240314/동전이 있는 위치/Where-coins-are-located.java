348f8b5ff3fedfe575b388d755b634578d382623
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-14 14:50:03 +0900

    Title: 동전이 있는 위치, Time: 163ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240314/동전이 있는 위치/Where-coins-are-located.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] arr = new int[n][n];
+
+        for(int i = 0; i < m; i++){
+            int r = sc.nextInt() - 1;
+            int c = sc.nextInt() - 1;
+            arr[r][c] = 1;
+        }
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++)
+                System.out.printf("%d ", arr[i][j]);
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
