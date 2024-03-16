5ff88f89ba5efbff7b4c499e7412965187231dc2
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-16 19:25:53 +0900

    Title: 정수만 추출하기, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240316/정수만 추출하기/extract-only-integers.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.next();
+        String s2 = sc.next();
+        String s1_num = "", s2_num = "";
+
+        for(int i = 0; i < s1.length(); i++){
+            if(s1.charAt(i) >= '0' && s1.charAt(i) <= '9')
+                s1_num += s1.charAt(i);
+            else
+                break;
+        }
+
+        for(int i = 0; i < s2.length(); i++){
+            if(s2.charAt(i) >= '0' && s2.charAt(i) <= '9')
+                s2_num += s2.charAt(i);
+            else
+                break;
+        }
+
+        System.out.print(Integer.parseInt(s1_num) + Integer.parseInt(s2_num));
+    }
+}
