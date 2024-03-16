3b28c506b0dd332514cb20dcaba38ebf04f2bcb7
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-16 18:20:14 +0900

    Title: 소문자와 숫자, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240316/소문자와 숫자/letter-and-number.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) >= 'A' && str.charAt(i) <= 'Z' || str.charAt(i) >= 'a' && str.charAt(i) <= 'z'){
+                if(str.charAt(i) >= 'A' && str.charAt(i) <= 'Z')
+                    System.out.print((char)(str.charAt(i) + 32));
+                else
+                    System.out.print(str.charAt(i));
+            }
+            else if(str.charAt(i) >= 48 && str.charAt(i) <= 57)
+                System.out.print(str.charAt(i));
+        
+        }
+    }
+}
