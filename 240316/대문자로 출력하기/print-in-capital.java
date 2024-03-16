1665a2f2ff376bc4c8bc7300d1f9fc9f524e75a0
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-16 17:12:32 +0900

    Title: 대문자로 출력하기, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 240316/대문자로 출력하기/print-in-capital.java
+++ 240316/대문자로 출력하기/print-in-capital.java
@@ -7,7 +7,7 @@ public class Main {
 
         for(int i = 0; i < str.length(); i++){
             if((str.charAt(i) >= 'a' && str.charAt(i) <= 'z') || (str.charAt(i) >= 'A' && str.charAt(i) <= 'Z')){
-                if(str.charAt(i) > 'a' && str.charAt(i) < 'z')
+                if(str.charAt(i) >= 'a' && str.charAt(i) <= 'z')
                     System.out.print((char)(str.charAt(i)-32));
                 else
                     System.out.print(str.charAt(i));
