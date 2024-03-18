383ec28e81fe847509a81c33679e708576c8eeb2
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-18 20:00:21 +0900

    Title: 특정 구간의 합, Time: 172ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240318/특정 구간의 합/sum-of-specific-intervals.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int n;
+    public static int[] arr = new int[100]; 
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int m = sc.nextInt();
+        int cnt = 0;
+
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        while(cnt < m){
+            int a1 = sc.nextInt();
+            int a2 = sc.nextInt();
+            System.out.println(sumA1toA2(a1, a2));
+            cnt++;
+        }
+
+    }
+    
+    // 구간의 합 구하는 함수
+    public static int sumA1toA2(int a1, int a2){
+        int sum = 0;
+        for(int i = a1-1; i < a2; i++)
+            sum += arr[i];
+        return sum;
+    }
+}
