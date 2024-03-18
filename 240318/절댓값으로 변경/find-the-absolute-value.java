222c86329ece51a604d39fad3891c7479c7c639f
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-18 16:00:38 +0900

    Title: 절댓값으로 변경, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 240318/절댓값으로 변경/find-the-absolute-value.java
+++ 240318/절댓값으로 변경/find-the-absolute-value.java
@@ -15,6 +15,7 @@ public class Main {
         }
     }
 
+    // 절대값으로 변경해주는 함수
     public static int[] absolute(int[] arr) {
         for(int i = 0; i < arr.length; i++){
             if(arr[i] < 0)
