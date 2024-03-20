6f3091391cdf76744a074d941224977b83547688
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-20 13:28:13 +0900

    Title: 007, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240320/007/007.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String code = sc.next();
+        char place = sc.next().charAt(0);
+        int time = sc.nextInt();
+        SecretCode s1 = new SecretCode(code, place, time);
+        System.out.printf("secret code : %s\nmeeting point : %c\ntime : %d", code, place, time);
+    }
+}
+
+class SecretCode {
+    String code;
+    char place;
+    int time;
+
+    public SecretCode(String code, char place, int time) {
+        this.code = code;
+        this.place = place;
+        this.time = time;
+    }
+}
