74146d95d5d963155dc83231401becca4edbde8e
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-20 13:11:27 +0900

    Title: k번째로 신기한 문자열, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240320/k번째로 신기한 문자열/kth-special-string.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        String t = sc.next();
+        String[] arr = new String[n];
+        int cnt = 0;
+
+        for(int i = 0; i < n; i++){
+            String str = sc.next();
+            if(str.contains(t)){
+                arr[cnt] = str;
+                cnt++;
+            }
+        }
+
+        Arrays.sort(arr, 0, cnt);
+        System.out.print(arr[k-1]);
+    }
+}
