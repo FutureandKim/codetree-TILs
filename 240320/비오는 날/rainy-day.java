fd3af0edc5fda0673431a550c56c3c6e2779d1a0
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-20 14:58:27 +0900

    Title: 비오는 날, Time: 168ms, Memory: 10MB, Status: Passed - CodeTree

--- 240320/비오는 날/rainy-day.java
+++ 240320/비오는 날/rainy-day.java
@@ -4,46 +4,21 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        WeatherInfo[] w = new WeatherInfo[n];
 
         for(int i = 0; i < n; i++){
             String date = sc.next();
             String day = sc.next();
             String weather = sc.next();
-            w[i] = new WeatherInfo(date, day, weather);
-        }
-
-        int min = 2101;
-        int cnt = 0;
-        for(int i = 0; i < n; i++){
-            if(w[i].weather.equals("Rain")){
-                int y = Integer.valueOf(w[i].date.substring(0,4));
-                if(y < min)
-                    min = y;
+            WeatherInfo w = new WeatherInfo(date, day, weather);
+            if(w.weather.equals("Rain")){
+                if(res.date.compareTo(w.date) > 0)
+                    res = w;
             }
         }
-
-        int minM = 13;
-        for(int i = 0; i < n; i++){
-            if(w[i].weather.equals("Rain") && Integer.valueOf(w[i].date.substring(0,4)) == min){
-                int m = Integer.valueOf(w[i].date.substring(5, 7));
-                if(m < minM)
-                    minM = m;
-            }
-        }
-
-        int minD = 32;
-        for(int i = 0; i < n; i++){
-            if(w[i].weather.equals("Rain") && Integer.valueOf(w[i].date.substring(0,4)) == min && Integer.valueOf(w[i].date.substring(5, 7)) == minM){
-                int d = Integer.valueOf(w[i].date.substring(8));
-                if(d < minD){
-                    minD = d;
-                    cnt = i;
-                }
-            }
-        }
-        System.out.printf("%s %s %s", w[cnt].date, w[cnt].day, w[cnt].weather);
+        System.out.printf("%s %s %s", res.date, res.day, res.weather);
     }
+
+    public static WeatherInfo res = new WeatherInfo("9999-99-99", "", "");
 }
 
 class WeatherInfo {
