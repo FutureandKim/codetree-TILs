7cd251d59d8611bfb09a4909f0bb63348a8333c2
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-20 14:54:31 +0900

    Title: 비오는 날, Time: 171ms, Memory: 10MB, Status: Passed - CodeTree

--- 240320/비오는 날/rainy-day.java
+++ 240320/비오는 날/rainy-day.java
@@ -18,12 +18,29 @@ public class Main {
         for(int i = 0; i < n; i++){
             if(w[i].weather.equals("Rain")){
                 int y = Integer.valueOf(w[i].date.substring(0,4));
-                if(y < min){
+                if(y < min)
                     min = y;
+            }
+        }
+
+        int minM = 13;
+        for(int i = 0; i < n; i++){
+            if(w[i].weather.equals("Rain") && Integer.valueOf(w[i].date.substring(0,4)) == min){
+                int m = Integer.valueOf(w[i].date.substring(5, 7));
+                if(m < minM)
+                    minM = m;
+            }
+        }
+
+        int minD = 32;
+        for(int i = 0; i < n; i++){
+            if(w[i].weather.equals("Rain") && Integer.valueOf(w[i].date.substring(0,4)) == min && Integer.valueOf(w[i].date.substring(5, 7)) == minM){
+                int d = Integer.valueOf(w[i].date.substring(8));
+                if(d < minD){
+                    minD = d;
                     cnt = i;
                 }
             }
-
         }
         System.out.printf("%s %s %s", w[cnt].date, w[cnt].day, w[cnt].weather);
     }
