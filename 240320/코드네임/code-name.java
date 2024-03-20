4caad6956ad8ebf78bbc4b5bad452c1d8e4766b6
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-20 13:51:13 +0900

    Title: 코드네임, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

--- 240320/코드네임/code-name.java
+++ 240320/코드네임/code-name.java
@@ -6,11 +6,15 @@ public class Main {
         Agent[] Agents = new Agent[5];
         char minName = 'A';
         int min = 101;
+
+        // 요원 정보 입력
         for(int i = 0; i < 5; i++){
             char name = sc.next().charAt(0);
             int score = sc.nextInt();
             Agents[i] = new Agent(name, score);
         }
+
+        // 점수가 제일 낮은 요원 찾기
         for(int i = 0; i < 5; i++){
             if(Agents[i].score < min){
                 minName = Agents[i].name;
