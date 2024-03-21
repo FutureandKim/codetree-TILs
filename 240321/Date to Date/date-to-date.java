da8c966f240cc57faaa4c171006043a9ff717db4
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-21 16:21:22 +0900

    Title: Date to Date, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

--- 240321/Date to Date/date-to-date.java
+++ 240321/Date to Date/date-to-date.java
@@ -9,28 +9,25 @@ public class Main {
         int d2 = sc.nextInt();
         int from = d1, to = d2;
 
-        for(int i = 1; i <= m1; i++){
+        for(int i = 1; i < m1; i++){
             if(i == 1 || i == 3 || i == 5 || i == 7 || i == 8 || i == 10 || i == 12)
                 from += 31;
             else if(i == 2)
                 from += 28;
-            else{
+            else if(i == 4 || i == 6 || i == 9 || i == 11){
                 from += 30;
             }
         }
 
-        for(int i = 1; i <= m2; i++){
+        for(int i = 1; i < m2; i++){
             if(i == 1 || i == 3 || i == 5 || i == 7 || i == 8 || i == 10 || i == 12)
                 to += 31;
-            else if(i == 2)
+            else if (i == 2)
                 to += 28;
-            else{
+            else if (i == 4 || i == 6 || i == 9 || i == 11){
                 to += 30;
             }
         }
-        if(to == from)
-            System.out.print(1);
-        else   
-            System.out.print(to-from);
+        System.out.print(to-from+1);
     }
 }
