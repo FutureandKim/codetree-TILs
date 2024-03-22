30b4e407b0b085c0f423a527a65befb34054ea59
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-23 00:09:00 +0900

    Title: 블럭쌓는 명령2, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

--- 240323/블럭쌓는 명령2/block-stacking-commands2.java
+++ 240323/블럭쌓는 명령2/block-stacking-commands2.java
@@ -6,26 +6,20 @@ public class Main {
         int n = sc.nextInt();
         int k = sc.nextInt();
         int[] blockNum = new int[n];
+        int num = 1;
 
         for(int i = 0; i < k; i++){
             int a = sc.nextInt();
             int b = sc.nextInt();
-            if(a==b)
-                blockNum[a-1] += 1;
-            else{
-                for(int j = a-1; j < b; j++)
-                    blockNum[j] += 1;
-            }
+            for(int j = a-1; j < b; j++)
+                blockNum[j] += 1;
         }
 
         int max = -1;
-        int cnt = -1;
-        for(int i = 0; i < k; i++){
-            if(blockNum[i] > max){
+        for(int i = 0; i < n; i++){
+            if(blockNum[i] > max)
                 max = blockNum[i];
-                cnt = i;
-            }
         }
-        System.out.print(cnt);
+        System.out.print(max);
     }
 }
