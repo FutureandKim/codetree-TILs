6646170f8f0c5f6bf590f7e615042e5fa1fda91b
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-22 10:23:26 +0900

    Title: 그 요일은, Time: 120ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240322/그 요일은/the-day-of-the-day.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+        int from = d1, to = d2;
+        String[] day = new String[]{ "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" };
+    
+        for(int i = 1; i < m1; i++){
+            if(i == 1 || i == 3 || i == 5 || i == 7 || i == 8 || i == 10 || i == 12)
+                from += 31;
+            else if(i == 2)
+                from += 29;
+            else if(i == 4 || i == 6 || i == 9 || i == 11)
+                from += 30;
+        }
+
+        for(int i = 1; i < m2; i++){
+            if(i == 1 || i == 3 || i == 5 || i == 7 || i == 8 || i == 10 || i == 12)
+                to += 31;
+            else if(i == 2)
+                to += 29;
+            else if(i == 4 || i == 6 || i == 9 || i == 11)
+                to += 30;
+        }
+
+        System.out.print((to-from)/7 + 1);
+    }
+}
