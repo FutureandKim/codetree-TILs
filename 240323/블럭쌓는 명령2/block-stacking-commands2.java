759d38d2dfda8e21f2f29dac96d6ae5d5bbfdb20
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-23 00:03:25 +0900

    Title: 블럭쌓는 명령2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240323/블럭쌓는 명령2/block-stacking-commands2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] blockNum = new int[n];
+
+        for(int i = 0; i < k; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            for(int j = a-1; j < b; j++)
+                blockNum[j] += 1;
+        }
+
+        int max = -1;
+        int cnt = -1;
+        for(int i = 0; i < k; i++){
+            if(blockNum[i] > max){
+                max = blockNum[i];
+                cnt = i;
+            }
+        }
+        System.out.print(cnt);
+    }
+}
