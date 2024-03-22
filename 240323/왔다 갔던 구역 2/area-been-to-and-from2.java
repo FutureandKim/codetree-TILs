6ff410ad841e73f049bfb50d8074a9fd4fe80160
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-23 01:08:34 +0900

    Title: 왔다 갔던 구역 2, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240323/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 240323/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -4,26 +4,44 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] line = new int[200];
+        int[] line = new int[220];
+        int idx = 100;
 
         for(int i = 0; i < n; i++){
             int x = sc.nextInt();
             char c = sc.next().charAt(0);
             if(c == 'R'){
-                for(int j = 100; j <= 100+x; j++)
-                    line[j] += 1;
+                for(int j = 0; j < x; j++){
+                    line[idx] += 1;
+                    idx++;
+                }
+                line[idx] += 1;
             }
             else if(c == 'L'){
-                for(int j = 100+x; j >= 100; j--)
-                    line[j] += 1;
+                for(int j = 0; j < x; j++){
+                    line[idx] += 1;
+                    idx--;
+                }
+                line[idx] += 1;
             }
         }
 
-        int max = -1;
-        for(int i = 0; i < 200; i++){
-            if(line[i] > max)
-                max = line[i];
+        int cnt = 0;
+        int[] visit = new int[110];
+
+        // 2번이상 방문한 점 찾기
+        for(int i = 0; i < 220; i++){
+            if(line[i] >= 2){
+                visit[cnt] = i;
+                cnt++;
+            }
         }
-        System.out.print(max);
+
+        int res = 0;
+        for(int i = 1; i < cnt; i++){
+            if(visit[i] == visit[i-1]+1)
+                res += 1;
+        }
+        System.out.print(res);
     }
 }
