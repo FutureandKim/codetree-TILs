2bada1143ce281e993e8c8fd5abb3c07026a0cb9
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-24 20:06:08 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 3033ms, Memory: 10MB, Status: Time Limit Exceed - CodeTree

---
+++ 240324/가장 왼쪽에 있는 최댓값/leftmost-max-value.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int max = -1, idx = -1, cnt = 0;
+        int[] idxArr = new int[n];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+            if(arr[i] > max){
+                max = arr[i];
+                idxArr[0] = i+1;
+            }
+        }
+        
+        int nowIdx = -1;
+        while(true){
+            if(nowIdx == 0)
+                break;
+
+            max = -1;
+            idx = -1;
+            for(int i = 0; i < idxArr[cnt]-1; i++){
+                if(arr[i] > max){
+                    max = arr[i];
+                    nowIdx = i;
+                    cnt+=1;
+                    idxArr[cnt] = i+1; 
+                }
+            }
+        }
+
+        for(int i = 0; i <= cnt; i++)
+            System.out.print(idxArr[i] + " ");
+    }
+}
