18424625276c486e1df6a7edc69d4d61ab821f10
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-24 20:14:16 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 241ms, Memory: 14MB, Status: Passed - CodeTree

--- 240324/가장 왼쪽에 있는 최댓값/leftmost-max-value.java
+++ 240324/가장 왼쪽에 있는 최댓값/leftmost-max-value.java
@@ -8,32 +8,28 @@ public class Main {
         int max = -1, idx = -1, cnt = 0;
         int[] idxArr = new int[n];
 
-        int nowIdx = -1;
         for(int i = 0; i < n; i++){
             arr[i] = sc.nextInt();
             if(arr[i] > max){
                 max = arr[i];
-                nowIdx = i;
                 idxArr[0] = i+1;
             }
         }
         
         while(true){
-            if(nowIdx == 0)
+            if(idxArr[cnt] == 1)
                 break;
 
             max = -1;
             idx = -1;
-            for(int i = 0; i < idxArr[cnt]-1; i++){
+            cnt++;
+            for(int i = 0; i < idxArr[cnt-1]-1; i++){
                 if(arr[i] > max){
                     max = arr[i];
-                    nowIdx = i;
-                    cnt+=1;
                     idxArr[cnt] = i+1; 
                 }
             }
         }
-
         for(int i = 0; i <= cnt; i++)
             System.out.print(idxArr[i] + " ");
     }
