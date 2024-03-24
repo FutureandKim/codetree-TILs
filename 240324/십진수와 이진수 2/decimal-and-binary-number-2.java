c93db918d3813638c709e81a04d25bb9e0725b58
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-24 18:17:46 +0900

    Title: 십진수와 이진수 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240324/십진수와 이진수 2/decimal-and-binary-number-2.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String n_str = Integer.toString(n);
+        int[] binaryArr = new int[n_str.length()]; // 2진수 저장할 배열
+
+        for(int i = binaryArr.length-1; i >= 0; i--){
+            binaryArr[i] = n%10;
+            n /= 10;
+        }
+
+        // 10진수 변환
+        int decimalNum = 0;
+        for(int i = 0; i < binaryArr.length; i++){
+            decimalNum = decimalNum * 2 + binaryArr[i];
+        }
+        decimalNum *= 17;
+        
+        // 2진수 변환
+        int[] digit = new int[20];
+        int cnt = 0;
+        while (true) {
+            if(decimalNum < 2)
+                digit[cnt++] = decimalNum;
+
+            digit[cnt++] = n % 2;
+            n /= 2;
+        }
+
+        for(int i = cnt-1; i >= 0; i--)
+            System.out.print(digit[i]);
+
+    }
+}
