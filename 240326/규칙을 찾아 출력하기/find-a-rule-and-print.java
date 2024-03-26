c478523458aa7007e9a57b4076923770ca7e1b87
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-26 18:31:44 +0900

    Title: 규칙을 찾아 출력하기, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

--- 240326/규칙을 찾아 출력하기/find-a-rule-and-print.java
+++ 240326/규칙을 찾아 출력하기/find-a-rule-and-print.java
@@ -5,15 +5,15 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        for(int i = 0; i < n; i++)
-            System.out.print("* ");
-
         for(int i = 0; i <= n-1; i++){
-            for(int j = 0; j <= n-1; j++)
-                if(j < i || (i > 0 && j == n-1))
+            for(int j = 0; j <= n-1; j++){
+                if(i == 0)
+                    System.out.print("* ");
+                else if(j < i || j == n-1)
                     System.out.print("* ");
                 else
                     System.out.print("  ");
+            }
             System.out.println();
         }
     }
