a5af0eb221f1293e1aecdd9d6bae665e7d084e24
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-27 13:53:41 +0900

    Title: 문자에 따른 명령 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240327/문자에 따른 명령 2/text-based-commands2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int[] dx = new int[]{ 1, 0, -1, 0 };
+        int[] dy = new int[]{ 0, -1, 0, 1 };
+        int stand = 3;
+        int xLoc = 0, yLoc = 0;
+
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == 'L')
+                stand--;
+            else if(s.charAt(i) == 'R')
+                stand++;
+            else if(s.charAt(i) == 'F'){
+                xLoc += dx[stand%4];
+                yLoc += dy[stand%4];
+            }
+        }
+        System.out.printf("%d %d", xLoc, yLoc);
+
+    }
+}
