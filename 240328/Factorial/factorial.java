25f5562eec3bff1fa88a84edfa5e2f7833a55252
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-28 19:28:48 +0900

    Title: Factorial, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240328/Factorial/factorial.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(factorial(n));
+    }
+
+    public static int factorial(int n) {
+        if(n == 0 || n == 1)
+            return 1;
+        n--;
+        return factorial(n) * (n+1);
+    }
+}
