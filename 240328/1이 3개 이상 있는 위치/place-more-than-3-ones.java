4928cebb0a883f9342e155ece3362682d8dc2701
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-28 18:54:15 +0900

    Title: 1이 3개 이상 있는 위치, Time: 268ms, Memory: 18MB, Status: Passed - CodeTree

--- 240328/1이 3개 이상 있는 위치/place-more-than-3-ones.java
+++ 240328/1이 3개 이상 있는 위치/place-more-than-3-ones.java
@@ -13,19 +13,19 @@ public class Main {
         }
 
         for(int i = 0; i < n; i++){
-            int isOne = 0;
             for(int j = 0; j < n; j++){
+                int isOne = 0;
                 if(i > 0 && grid[i-1][j] == 1)
                     isOne++;
                 if(i < n-1 && grid[i+1][j] == 1)
                     isOne++;
                 if(j > 0 && grid[i][j-1] == 1)
                     isOne++;
-                if(j < n-1 &&  grid[i][j+1] == 1)
+                if(j < n-1 && grid[i][j+1] == 1)
                     isOne++;
+                if(isOne >= 3)
+                    cnt++;
             }
-            if(isOne >= 3)
-                cnt++;
         }
         System.out.print(cnt);
     }
