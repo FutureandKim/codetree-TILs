efc73dd1a1b855a02e9b113229a434fc9449102c
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-28 19:29:48 +0900

    Title: Factorial, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

--- 240328/Factorial/factorial.java
+++ 240328/Factorial/factorial.java
@@ -10,7 +10,6 @@ public class Main {
     public static int factorial(int n) {
         if(n == 0 || n == 1)
             return 1;
-        n--;
-        return factorial(n) * (n+1);
+        return factorial(n-1) * n;
     }
 }
