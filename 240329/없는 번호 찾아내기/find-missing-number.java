6355efd24d362ef09fce50a81587b8463017c095
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-29 21:51:49 +0900

    Title: 없는 번호 찾아내기, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

--- 240329/없는 번호 찾아내기/find-missing-number.java
+++ 240329/없는 번호 찾아내기/find-missing-number.java
@@ -6,10 +6,12 @@ public class Main {
         int[] arr = new int[28];
         int[] number = new int[30];
         
+        // 28개 정수 입력
         for(int i = 0; i < 28; i++){
             arr[i] = sc.nextInt();
         }
 
+        // number 배열을 돌면서 숫자가 존재한다면 number[j] = 1 로 설정
         for(int i = 0; i < 28; i++){
             for(int j = 0; j < 30; j++){
                 if(arr[i] == j+1)
@@ -21,7 +23,6 @@ public class Main {
             if(number[j] == 0)
                 System.out.println(j+1);
         }
-
     
     }
 }
