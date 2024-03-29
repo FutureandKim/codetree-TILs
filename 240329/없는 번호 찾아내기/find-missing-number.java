720f03c0aab931c2e5c0b38b68209c86356860bc
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-29 21:50:27 +0900

    Title: 없는 번호 찾아내기, Time: 162ms, Memory: 10MB, Status: Passed - CodeTree

--- 240329/없는 번호 찾아내기/find-missing-number.java
+++ 240329/없는 번호 찾아내기/find-missing-number.java
@@ -1,20 +1,27 @@
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] arr = new int[28];
+        int[] number = new int[30];
         
         for(int i = 0; i < 28; i++){
             arr[i] = sc.nextInt();
         }
 
-        Arrays.sort(arr);
-
-        for(int i = 0; i < 27; i++){
-            if(arr[i+1] - arr[i] != 1)
-                System.out.println(arr[i] + 1);
+        for(int i = 0; i < 28; i++){
+            for(int j = 0; j < 30; j++){
+                if(arr[i] == j+1)
+                    number[j] = 1;
+            }
         }
+
+        for(int j = 0; j < 30; j++){
+            if(number[j] == 0)
+                System.out.println(j+1);
+        }
+
+    
     }
 }
