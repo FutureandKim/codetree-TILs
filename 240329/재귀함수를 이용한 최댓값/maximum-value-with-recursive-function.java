2d24d81003056c83ff0220f480ea674517f8fec6
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-29 16:17:57 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 235ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240329/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    static int[] numArr;
+    static int n;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        numArr = new int[n];
+
+        for(int i = 0; i < n; i++)
+            numArr[i] = sc.nextInt();
+        
+        int max = maxNum(0);
+        System.out.print(max);
+    }
+
+    public static int maxNum(int idx){
+        if(idx == n-1)
+            return numArr[idx];
+        return Math.max(numArr[idx], maxNum(idx+1));
+    }
+}
