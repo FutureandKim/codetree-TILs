f630189e5220253b71df4fa1c651e0444e1ec13d
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-29 22:32:46 +0900

    Title: 십진수와 이진수, Time: 169ms, Memory: 10MB, Status: Passed - CodeTree

--- 240329/십진수와 이진수/decimal-and-binary-number.java
+++ 240329/십진수와 이진수/decimal-and-binary-number.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
+import java.math.BigInteger;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String n = sc.next();
 
-        int decimalNum = Integer.parseInt(n, 2);
-        decimalNum *= 17;
-        String binary = Integer.toString(decimalNum, 2);
+        BigInteger decimalNum = new BigInteger(n, 2);
+        decimalNum = decimalNum.multiply(BigInteger.valueOf(17));
+        String binary = decimalNum.toString(2);
+
         System.out.print(binary);
     }
 }
