8677aaad33178d67df7057619e6fc8808b2e3202
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-30 22:20:50 +0900

    Title: 진법 변환 3, Time: 676ms, Memory: 26MB, Status: Passed - CodeTree

--- 240330/진법 변환 3/base-conversion-3.java
+++ 240330/진법 변환 3/base-conversion-3.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
+import java.math.BigInteger;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String n = sc.next();
 
-        int dec = Integer.parseInt(n, 8);
-        String bin = Integer.toString(dec, 2);
+        BigInteger dec = new BigInteger(n, 8);
+        String bin = dec.toString(2);
         System.out.print(bin);
     }
 }
