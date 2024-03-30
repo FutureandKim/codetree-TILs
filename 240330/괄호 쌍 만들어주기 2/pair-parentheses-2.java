9fdd5e9666f5bbf2f117689827921f53b8455dd8
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-30 14:08:22 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240330/괄호 쌍 만들어주기 2/pair-parentheses-2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int cnt = 0;
+
+        for(int i = 0; i < str.length()-1; i++){
+            if(str.charAt(i) == '(' && str.charAt(i+1) == '('){
+                for(int j = i+1; j < str.length()-1; j++){
+                    if(str.charAt(j) == ')' && str.charAt(j+1) == ')')
+                        cnt++;
+                }
+            }
+        }
+        System.out.print(cnt);
+    }
+}
