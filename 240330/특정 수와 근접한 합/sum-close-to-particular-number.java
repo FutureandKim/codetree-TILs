7602d134e74909f544ace58542841cedac578b66
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-30 14:24:32 +0900

    Title: 특정 수와 근접한 합, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

--- 240330/특정 수와 근접한 합/sum-close-to-particular-number.java
+++ 240330/특정 수와 근접한 합/sum-close-to-particular-number.java
@@ -5,20 +5,20 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int s = sc.nextInt();
-        int[] arr = new int[n];
-
-        int len = (n*(n-1))/2;
-        int[] sum = new int[len];
-        int allSum = 0, cnt = 0;
+        int[] arr = new int[n]; // n개의 정수 배열
+        int len = (n*(n-1))/2; // (n-2)개 정수합 개수(nC2)
+        int[] sum = new int[len]; // (n-2)개 정수합 저장할 배열
+        int allSum = 0, idx = 0;
 
         for(int i = 0; i < n; i++){
             arr[i] = sc.nextInt();
             allSum += arr[i];
         }
 
+        // (n-2)개 정수합 == 전체 합 - (2개의 합)
         for(int i = 0; i < n; i++){
             for(int j = i+1; j < n; j++)
-                sum[cnt++] = allSum - (arr[i] + arr[j]);
+                sum[idx++] = allSum - (arr[i] + arr[j]);
         }
 
         int res = Integer.MAX_VALUE;
