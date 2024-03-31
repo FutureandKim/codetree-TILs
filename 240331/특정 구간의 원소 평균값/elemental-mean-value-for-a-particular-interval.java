a422b8f1b5fefad9ee710811f2c0e907341df28a
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-31 16:33:42 +0900

    Title: 특정 구간의 원소 평균값, Time: 158ms, Memory: 10MB, Status: Passed - CodeTree

--- 240331/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.java
+++ 240331/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
-        int max = Integer.MIN_VALUE, cnt = 0;
+        int cnt = 0;
 
         for(int i = 0; i < n; i++)
             arr[i] = sc.nextInt();
