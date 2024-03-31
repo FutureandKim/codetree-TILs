9064578f1a7d5f3bb936d05904ef464b9e24d9f2
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-31 23:10:33 +0900

    Title: 오목, Time: 151ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240331/오목/O-mok.java
+++ 240331/오목/O-mok.java
@@ -11,8 +11,9 @@ public class Main {
                 omok[i][j] = sc.nextInt();
         }
 
-        for(int i = 0; i < 14; i++){
-            for(int j = 0; j < 14; j++){
+        for(int i = 0; i < 15; i++){
+            for(int j = 0; j < 15; j++){
+                // 가로
                 if(omok[i][j] == 1 && omok[i][j+1] == 1 && omok[i][j+2] == 1 && omok[i][j+3] == 1 && omok[i][j+4] == 1){
                     System.out.printf("%d\n%d %d", 1, i+1, j+3);
                     isWin = true;
@@ -23,6 +24,7 @@ public class Main {
                     isWin = true;
                     break;
                 }
+                // 세로
                 else if(omok[j][i] == 1 && omok[j+1][i] == 1 && omok[j+2][i] == 1 && omok[j+3][i] == 1 && omok[j+4][i] == 1){
                     System.out.printf("%d\n%d %d", 1, j+3, i+1);
                     isWin = true;
@@ -33,14 +35,32 @@ public class Main {
                     isWin = true;
                     break;
                 }
+                // 대각선 (y=-x 방향)
                 else if(omok[i][j] == 1 && omok[i+1][j+1] == 1 && omok[i+2][i+2] == 1 && omok[i+3][j+3] == 1 && omok[i+4][j+4] == 1){
                     System.out.printf("%d\n%d %d", 1, i+3, j+3);
                     isWin = true;
                     break;
                 }
-                else if(omok[i][j] == 2 && omok[i+1][j+1] == 2 && omok[i+2][i+2] == 2 && omok[i+3][j+3] == 2 && omok[i+4][j+4] == 2){
+                else if(omok[i][j] == 1 && omok[i+1][j+1] == 2 && omok[i+2][i+2] == 2 && omok[i+3][j+3] == 2 && omok[i+4][j+4] == 2){
                     System.out.printf("%d\n%d %d", 2, i+3, j+3);
                     isWin = true;
+                    break;
+                }
+            }
+        }
+
+        for(int i = 5; i < 19; i++){
+            for(int j = 0; j < 15; j++){
+                // 대각선 (y=x 방향)
+                if(omok[i][j] == 1 && omok[i-1][j+1] == 1 && omok[i-2][j+2] == 1 && omok[i-3][j+3] == 1 && omok[i-4][j+4] == 1){
+                    System.out.printf("%d\n%d %d", 2, i-1, j+3);
+                    isWin = true;
+                    break;
+                }
+                else if(omok[i][j] == 2 && omok[i-1][j+1] == 2 && omok[i-2][j+2] == 2 && omok[i-3][j+3] == 2 && omok[i-4][j+4] == 2){
+                    System.out.printf("%d\n%d %d", 2, i-1, j+3);
+                    isWin = true;
+                    break;
                 }
             }
         }
