66cc717b421195fd524327974ac268a6a0e35d30
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-31 23:17:33 +0900

    Title: 오목, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240331/오목/O-mok.java
+++ 240331/오목/O-mok.java
@@ -11,7 +11,7 @@ public class Main {
                 omok[i][j] = sc.nextInt();
         }
 
-        for(int i = 0; i < 15; i++){
+        for(int i = 0; i < 19; i++){
             for(int j = 0; j < 15; j++){
                 // 가로
                 if(omok[i][j] == 1 && omok[i][j+1] == 1 && omok[i][j+2] == 1 && omok[i][j+3] == 1 && omok[i][j+4] == 1){
@@ -24,17 +24,6 @@ public class Main {
                     isWin = true;
                     break;
                 }
-                // 세로
-                else if(omok[j][i] == 1 && omok[j+1][i] == 1 && omok[j+2][i] == 1 && omok[j+3][i] == 1 && omok[j+4][i] == 1){
-                    System.out.printf("%d\n%d %d", 1, j+3, i+1);
-                    isWin = true;
-                    break;
-                }
-                else if(omok[j][i] == 2 && omok[j+1][i] == 2 && omok[j+2][i] == 2 && omok[j+3][i] == 2 && omok[j+4][i] == 2){
-                    System.out.printf("%d\n%d %d", 2, j+3, i+1);
-                    isWin = true;
-                    break;
-                }
                 // 대각선 (y=-x 방향)
                 else if(omok[i][j] == 1 && omok[i+1][j+1] == 1 && omok[i+2][i+2] == 1 && omok[i+3][j+3] == 1 && omok[i+4][j+4] == 1){
                     System.out.printf("%d\n%d %d", 1, i+3, j+3);
@@ -43,6 +32,23 @@ public class Main {
                 }
                 else if(omok[i][j] == 1 && omok[i+1][j+1] == 2 && omok[i+2][i+2] == 2 && omok[i+3][j+3] == 2 && omok[i+4][j+4] == 2){
                     System.out.printf("%d\n%d %d", 2, i+3, j+3);
+                    isWin = true;
+                    break;
+                }
+            }
+        }
+
+
+        // 세로
+        for(int i = 0; i < 19; i++){
+            for(int j = 0; j < 15; j++){
+                if(omok[j][i] == 1 && omok[j+1][i] == 1 && omok[j+2][i] == 1 && omok[j+3][i] == 1 && omok[j+4][i] == 1){
+                    System.out.printf("%d\n%d %d", 1, j+3, i+1);
+                    isWin = true;
+                    break;
+                }
+                else if(omok[j][i] == 2 && omok[j+1][i] == 2 && omok[j+2][i] == 2 && omok[j+3][i] == 2 && omok[j+4][i] == 2){
+                    System.out.printf("%d\n%d %d", 2, j+3, i+1);
                     isWin = true;
                     break;
                 }
