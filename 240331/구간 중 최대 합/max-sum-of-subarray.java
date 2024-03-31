4ee0f323c820960c33b219b54c8b71a9d5811cfb
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-03-31 15:20:50 +0900

    Title: 구간 중 최대 합, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240331/구간 중 최대 합/max-sum-of-subarray.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        int hap, max = Integer.MIN_VALUE;
+
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i < n-3; i++){
+            hap = arr[i] + arr[i+1] + arr[i+2];
+            max = Math.max(hap, max);
+        }
+        System.out.print(max);
+    }
+}
