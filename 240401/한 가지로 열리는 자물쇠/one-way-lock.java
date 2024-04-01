461f6141c6215df1475fa719ea7f07da035a690a
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-04-01 15:57:29 +0900

    Title: 한 가지로 열리는 자물쇠, Time: 155ms, Memory: 10MB, Status: Passed - CodeTree

--- 240401/한 가지로 열리는 자물쇠/one-way-lock.java
+++ 240401/한 가지로 열리는 자물쇠/one-way-lock.java
@@ -12,6 +12,7 @@ public class Main {
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= n; j++){
                 for(int k = 1; k <= n; k++){
+                    // 조건이 만족하지 않는 경우 찾기
                     if(Math.abs(i-a) > 2 && Math.abs(j-b) > 2 && Math.abs(k-c) > 2)
                         cnt++;
                 }
