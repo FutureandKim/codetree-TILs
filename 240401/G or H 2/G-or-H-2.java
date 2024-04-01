46c9a45a94cfd42caaf0080bace330ecd38066a0
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-04-01 22:21:05 +0900

    Title: G or H 2, Time: 163ms, Memory: 10MB, Status: Passed - CodeTree

--- 240401/G or H 2/G-or-H-2.java
+++ 240401/G or H 2/G-or-H-2.java
@@ -1,46 +1,39 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         char[] loc = new char[101];
+        int idxArr[] = new int[n];
         int maxLoc = 0;
-        int len = 0, k;
+        int len = 0, k, cntG = 0, cntH = 0;
 
         for(int i = 0; i < n; i++){
             int idx = sc.nextInt();
             char c = sc.next().charAt(0);
             loc[idx] = c;
+            idxArr[i] = idx;
             maxLoc = Math.max(idx, maxLoc);
         }
 
+        Arrays.sort(idxArr);
+
         int max = 0;
-        int minG = Integer.MAX_VALUE, minH = Integer.MAX_VALUE;
-        int maxG = Integer.MIN_VALUE, maxH = Integer.MIN_VALUE;
-        int minS = Integer.MAX_VALUE, maxS = Integer.MIN_VALUE;
-        for(int i = 1; i < maxLoc-1; i++){
-            for(int j = 1; j < maxLoc-1; j++){
-                int cntG = 0, cntH = 0;
-                for(k = i; k <= j; k++){
-                    if(loc[k] == 'G'){
+        for(int i = 0; i < n; i++){   
+            for(int j = i+1; j < n; j++){
+                cntG = 0;
+                cntH = 0;
+                for(int z = i; z <= j; z++){
+                    if(loc[idxArr[z]] == 'G')
                         cntG++;
-                        minG = Math.min(minG, k);
-                        maxG = Math.max(maxG, k);
-                    }
-                    if(loc[k] == 'H'){
+                    if(loc[idxArr[z]] == 'H')
                         cntH++;
-                        minH = Math.min(minH, k);
-                        maxH = Math.max(maxH, k);
-                    }
                 }
-                //System.out.println(cntG +" " + cntH + " k:"+k + " i:"+i);
-                minS = Math.min(minG, minH);
-                maxS = Math.max(maxG, maxG);
-                if(cntG == 0 && cntH == 0)
-                    continue;
-                else if((cntG == 0 && cntH != 0) || (cntG != 0 && cntH == 0) || cntG == cntH)
-                    len = maxS-minS+1;
+                //System.out.println(cntG +" " + cntH + "| i:"+ idxArr[i] +  " j: "+ idxArr[j]);
+                if(cntG == cntH || (cntG == 0 && cntH != 0) || (cntG != 0 && cntH == 0))
+                    len = idxArr[j]-idxArr[i];
                 max = Math.max(max, len);
             }
         }
