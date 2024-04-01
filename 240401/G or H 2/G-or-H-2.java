02f6c3654b07d438508ed52e50ca788ceee0b321
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-04-01 22:22:41 +0900

    Title: G or H 2, Time: 152ms, Memory: 10MB, Status: Passed - CodeTree

--- 240401/G or H 2/G-or-H-2.java
+++ 240401/G or H 2/G-or-H-2.java
@@ -6,15 +6,15 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         char[] loc = new char[101];
-        int idxArr[] = new int[n];
+        int idxArr[] = new int[n]; // 사람 위치를 저장할 배열
         int maxLoc = 0;
-        int len = 0, k, cntG = 0, cntH = 0;
+        int len = 0, k;
 
         for(int i = 0; i < n; i++){
             int idx = sc.nextInt();
             char c = sc.next().charAt(0);
             loc[idx] = c;
-            idxArr[i] = idx;
+            idxArr[i] = idx; 
             maxLoc = Math.max(idx, maxLoc);
         }
 
@@ -23,15 +23,13 @@ public class Main {
         int max = 0;
         for(int i = 0; i < n; i++){   
             for(int j = i+1; j < n; j++){
-                cntG = 0;
-                cntH = 0;
+                int cntG = 0, cntH = 0;
                 for(int z = i; z <= j; z++){
                     if(loc[idxArr[z]] == 'G')
                         cntG++;
                     if(loc[idxArr[z]] == 'H')
                         cntH++;
                 }
-                //System.out.println(cntG +" " + cntH + "| i:"+ idxArr[i] +  " j: "+ idxArr[j]);
                 if(cntG == cntH || (cntG == 0 && cntH != 0) || (cntG != 0 && cntH == 0))
                     len = idxArr[j]-idxArr[i];
                 max = Math.max(max, len);
