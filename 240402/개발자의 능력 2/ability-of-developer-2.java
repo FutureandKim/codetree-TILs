3d60fdec1fb5c6d1332f99b654ad8f4e60a39368
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-04-02 15:48:36 +0900

    Title: 개발자의 능력 2, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

--- 240402/개발자의 능력 2/ability-of-developer-2.java
+++ 240402/개발자의 능력 2/ability-of-developer-2.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] ab = new int[6];
+        int[] group = new int[3]; // 팀별 능력 총합을 저장할 배열
 
         for(int i = 0; i < 6; i++)
             ab[i] = sc.nextInt();
@@ -13,17 +14,12 @@ public class Main {
 
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
-        
-        int sum1 = ab[0] + ab[5];
-        int sum2 = ab[1] + ab[4];
-        int sum3 = ab[2] + ab[3];
 
-        min = Math.min(sum1, sum2);
-        min = Math.min(min, sum3);
-
-        max = Math.max(sum1, sum2);
-        max = Math.max(max, sum3);
-        int res = max - min;
-        System.out.print(res);
+        for(int i = 0; i < 3; i++){
+            group[i] = ab[i] + ab[5-i];
+            min = Math.min(group[i], min);
+            max = Math.max(group[i], max);
+        }
+        System.out.print(max - min);
     }
 }
