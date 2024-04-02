cefc320a98fc149f70eee65980ac8da059c7d466
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-04-02 18:29:51 +0900

    Title: 숫자들의 합 중 최대, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240402/숫자들의 합 중 최대/maximum-of-sum-of-numbers.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int max = 0;
+
+        for(int i = x; i <= y; i++)
+            max = Math.max(max, sumOfEachDigit(i));
+        System.out.print(max);
+    }
+
+    public static int sumOfEachDigit(int n){
+        int sum = 0;
+        while(true){
+            if(n == 0)
+                break;
+            sum += (n%10);
+            n /= 10;
+        }
+        return sum;
+    }
+}
