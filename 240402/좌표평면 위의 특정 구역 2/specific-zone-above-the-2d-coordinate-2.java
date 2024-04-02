ca200681f017d65081d7f29fcea803509f91fb4e
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-04-02 16:46:51 +0900

    Title: 좌표평면 위의 특정 구역 2, Time: 161ms, Memory: 10MB, Status: Passed - CodeTree

--- 240402/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.java
+++ 240402/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.java
@@ -19,21 +19,23 @@ public class Main {
             int maxX = Integer.MIN_VALUE;
             int maxY = Integer.MIN_VALUE;
 
+            // i == j 인 경우 제외하고 X좌표 순환
             for(int j = 0; j < n; j++){
                 if(j != i){
-                minX = Math.min(minX, p[j][0]);
-                maxX = Math.max(maxX, p[j][0]);
+                    minX = Math.min(minX, p[j][0]);
+                    maxX = Math.max(maxX, p[j][0]);
                 }
             }        
-            //System.out.println(minX + " "+ maxX);
 
+            // i == j 인 경우 제외하고 Y좌표 순환
             for(int j = 0; j < n; j++){
                 if(j != i){
-                minY = Math.min(minY, p[j][1]);
-                maxY = Math.max(maxY, p[j][1]);
+                    minY = Math.min(minY, p[j][1]);
+                    maxY = Math.max(maxY, p[j][1]);
                 }
             }        
-            //System.out.println(minY + " "+ maxY);
+
+            // 사각형의 넓이
             int res = (maxX - minX) * (maxY - minY);
             minWidth = Math.min(minWidth, res);
         }
