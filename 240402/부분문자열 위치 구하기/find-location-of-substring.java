ee86c8966f6dba8261f5963c4f1c1fc7235d199b
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-04-02 21:16:16 +0900

    Title: 부분문자열 위치 구하기, Time: 157ms, Memory: 10MB, Status: Passed - CodeTree

--- 240402/부분문자열 위치 구하기/find-location-of-substring.java
+++ 240402/부분문자열 위치 구하기/find-location-of-substring.java
@@ -5,17 +5,19 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         String s1 = sc.next();
         String s2 = sc.next();
+        int cnt = 0;
+        int len1 = s1.length(), len2 = s2.length();
         boolean isExist = true;
-        int cnt = 0, len1 = s1.length(), len2 = s2.length();
 
         if(!s1.contains(s2))
             isExist = false;
         else {
             for(int i = 0; i < len1-len2+1; i++){
+                // s1의 i번째와 s2의 첫번째 문자가 일치하는지 확인
                 if(s1.charAt(i) == s2.charAt(0)){
                     int idx = 0;
                     for(int j = i; j < i+len2; j++){
-                        if(s1.charAt(j) != s2.charAt(idx))
+                        if(s1.charAt(j) != s2.charAt(idx)) 
                             break;
                         if(j == i+len2-1 && (s1.charAt(j) == s2.charAt(len2-1))){
                             cnt = i;
