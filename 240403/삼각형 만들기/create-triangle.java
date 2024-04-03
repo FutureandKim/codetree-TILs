a9040c942d3ace339d5390829b0e04f62ac66173
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-04-03 18:18:49 +0900

    Title: 삼각형 만들기, Time: 149ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240403/삼각형 만들기/create-triangle.java
+++ 240403/삼각형 만들기/create-triangle.java
@@ -2,47 +2,52 @@ import java.util.Scanner;
 
 public class Main {
     public static int[][] dot;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         dot = new int[n][2];
 
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < 2; j++)
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 2; j++)
                 dot[i][j] = sc.nextInt();
         }
 
-        int max = Integer.MIN_VALUE;
-        for(int i = 0; i < n-2; i++){
-            for(int j = i+1; j < n-1; j++){
-                for(int k = j+1; k < n; k++){
-                    max = Math.max(max, getTriangle(i, j, k));
+        int maxArea = 0;
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    int area = getTriangleArea(i, j, k);
+                    maxArea = Math.max(maxArea, area);
                 }
             }
         }
-        System.out.print(max);
+        System.out.print(maxArea * 2);
     }
 
-    // x축, y축에 평행한 삼각형을 찾고 (넓이x2)를 반환하는 함수
-    public static int getTriangle(int i, int j, int k){
+    // 한 변이 x축에 평행, 다른 변이 y축에 평행한 삼각형의 넓이를 계산하는 함수
+    public static int getTriangleArea(int i, int j, int k) {
         int x1 = dot[i][0], x2 = dot[j][0], x3 = dot[k][0];
         int y1 = dot[i][1], y2 = dot[j][1], y3 = dot[k][1];
-        boolean isTrue = false;
-        
-        if(x1 == x2 && x1 != x3){
-            if(y1 == y3 || y2 == y3)
-                isTrue = true;
-        }
-        else if(x1 == x3 && x1 != x3){
-            if(y1 == y2 || y3 == y2)
-                isTrue = true;
-        } else if(x2 == x3 && x1 != x2){
-            if(y2 == y1 || y3 == y2)
-                isTrue = true;
-        }
-        
-        if(!isTrue)
+        int width = 0, height = 0;
+
+        if ((x1 == x2 && x2 == x3) || (y1 == y2 && y2 == y3)) {
+            // 한 변이 x축 또는 y축에 평행한 삼각형이 아닌 경우
             return 0;
-        return ((x1 * y2) + (x2 * y3) + (x3 * y1)) - ((x2 * y1) + (x3 * y2) + (x1 * y3));
+        }
+
+        // 삼각형의 넓이를 계산
+        if (x1 == x2) {
+            height = Math.abs(y3 - y1);
+            width = Math.abs(x3 - x1);
+        } else if (x1 == x3) {
+            height = Math.abs(y2 - y1);
+            width = Math.abs(x2 - x1);
+        } else if (x2 == x3) {
+            height = Math.abs(y1 - y2);
+            width = Math.abs(x1 - x2);
+        }
+
+        return width * height * 1/2;
     }
 }
