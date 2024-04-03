5aaa6d74cacff220708d95c7745683d6f8cf3a47
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-04-03 20:34:53 +0900

    Title: 운행 되고 있는 시간, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

--- 240403/운행 되고 있는 시간/hours-in-service.java
+++ 240403/운행 되고 있는 시간/hours-in-service.java
@@ -7,32 +7,32 @@ public class Main {
         int[][] work = new int[n][2];
         int[] hour = new int[1001];
 
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < 2; j++)
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 2; j++)
                 work[i][j] = sc.nextInt();
-        }
-
-        for(int i = 0; i < n; i++){
-            int start = work[i][0];
-            int finish = work[i][1];
-            for(int j = start; j < finish; j++)
+            // 개발자가 일하는 시간 동안 배열 값을 1씩 증가
+            for (int j = work[i][0]; j < work[i][1]; j++) {
                 hour[j]++;
+            }
         }
 
         int max = Integer.MIN_VALUE;
-        for(int i = 0; i < n; i++){
-            int start = work[i][0];
-            int finish = work[i][1];
-            for(int j = start; j < finish; j++){
+        for (int i = 0; i < n; i++) {
+            // 해고된 개발자 시간 감소
+            for (int j = work[i][0]; j < work[i][1]; j++)
                 hour[j]--;
-                int cnt = 0;
-                for(int k = 0; k < 1001; k++){
-                    if(hour[k] != 0)
-                        cnt++;
-                }
-                max = Math.max(max, cnt);
+            int cnt = 0;
+            for (int k = 1; k < 1001; k++) {
+                if (hour[k] != 0)
+                    cnt++;
             }
+            max = Math.max(max, cnt);
+            // 다시 원래 배열 상태로 되돌리기
+            for (int j = work[i][0]; j < work[i][1]; j++)
+                hour[j]++;
         }
-        System.out.print(max-1);
+        System.out.print(max);
+
     }
+    
 }
