296200289c734c8430dd1f0f360765e0dd141c32
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-04-04 10:21:29 +0900

    Title: 데이터센터의 온도 조정 2, Time: 230ms, Memory: 14MB, Status: Passed - CodeTree

--- 240404/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.java
+++ 240404/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.java
@@ -10,13 +10,13 @@ public class Main {
         g = sc.nextInt();
         h = sc.nextInt();
         int[][] tempArr = new int[n][2];
+        int max = Integer.MIN_VALUE;
          
         for(int i = 0; i < n; i++){
             tempArr[i][0] = sc.nextInt();
             tempArr[i][1] = sc.nextInt();
         }
-
-        int max = Integer.MIN_VALUE;
+        
         for(int i = -10; i < 1100; i++){
             int work = 0;
             for(int j = 0; j < n; j++)
@@ -24,15 +24,6 @@ public class Main {
             max = Math.max(max, work);
         }
         System.out.print(max);
-        /*
-        for(int i = 0; i < n; i++){
-            int work = 0;
-            for(int j = -10; j < 1100; j++){
-                work += workload(j, tempArr[i][0], tempArr[i][1]);
-            }
-            max = Math.max(max, work);
-        }*/
-
     }
 
     public static int workload(int temp, int ta, int tb){
