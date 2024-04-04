8cfe361db15e4ee885adfdd497d0a2e285f37b2d
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-04-04 10:43:48 +0900

    Title: 정보에 따른 숫자 2, Time: 160ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240404/정보에 따른 숫자 2/number-based-on-information-2.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+    public static char[] loc = new char[1001];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        for(int i = 0; i < t; i++){
+            char c = sc.next().charAt(0);
+            int n = sc.nextInt();
+            loc[n] = c;
+        }
+
+        int cnt = 0;
+        for(int i = a; i <= b; i++)
+            cnt += isSpecialNum(i);
+        System.out.print(cnt);
+    }
+
+    public static int isSpecialNum(int n){
+        int minD1 = Integer.MAX_VALUE;
+        int minD2 = Integer.MAX_VALUE;
+
+        for(int i = 0; i < 1001; i++){
+            if(loc[i] == 'S')
+                minD1 = Math.min(minD1, Math.abs(n-i));
+            if(loc[i] == 'N')
+                minD2 = Math.min(minD2, Math.abs(n-i));        
+        }
+
+        if(minD1 <= minD2)
+            return 1;
+        return 0;
+    }
+}
