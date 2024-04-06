b902f98294b03469865c811f96e3e5c395913819
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-04-06 20:35:37 +0900

    Title: 두 직사각형, Time: 132ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240406/두 직사각형/two-rectangles.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x1 = sc.nextInt();
+        int y1 = sc.nextInt();
+        int x2 = sc.nextInt();
+        int y2 = sc.nextInt();
+
+        int a1 = sc.nextInt();
+        int b1 = sc.nextInt();
+        int a2 = sc.nextInt();
+        int b2 = sc.nextInt();
+
+        boolean isOverlap = true;
+
+        if(x2 < a1 || b2 < x1) isOverlap = false;
+        if(b2 < y1 || y2 < b2) isOverlap = false;
+
+        System.out.print(isOverlap ? "overlapping" : "nonoverlapping");
+    }
+}
