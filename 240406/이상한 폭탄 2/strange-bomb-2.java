db055bfe35cbdefdaf913a0c241bc59cebd41aaa
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-04-06 18:48:07 +0900

    Title: 이상한 폭탄 2, Time: 148ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240406/이상한 폭탄 2/strange-bomb-2.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] bomb = new int[n];
+        int max = Integer.MIN_VALUE;
+
+        for(int i = 0; i < n; i++)
+            bomb[i] = sc.nextInt();
+
+        for(int i = 0; i < n; i++){
+            int start = i-k;
+            int fin = i+k;
+            if((i-k) < 0)
+                start = 0;
+            if((i+k) >= n)
+                fin = n-1;
+
+            for(int j = start; j <= fin; j++){
+                if(i == j)
+                    continue;
+                else if(bomb[i] == bomb[j])
+                    max = Math.max(max, bomb[i]);
+            }
+        }
+        System.out.print(max);
+    }
+}
