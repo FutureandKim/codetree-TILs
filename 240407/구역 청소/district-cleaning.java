e90e6b44b8b84b9c5ba53e5cd35b13dda0b7fb43
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-04-07 15:40:38 +0900

    Title: 구역 청소, Time: 137ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240407/구역 청소/district-cleaning.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int len = 0;
+
+        // 겹치지 않는 경우
+        if(b < c || d < a)
+            len = (b-a) + (d-c);
+        // 겹치는 경우
+        else if(a <= c && c <= b)
+            len = d - a;
+        else if(c <= a && a <= d)
+            len = b - c;
+        // 한 직선이 다른 직선 내에 포함되는 경우
+        else if(a <= c && d <= b)
+            len = b - a;
+        else if(c <= a && b <= d)
+            len = d - c;
+
+        System.out.print(len);
+    }
+}
