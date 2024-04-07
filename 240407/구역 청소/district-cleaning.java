71fd00d257182092a7c3fea01e2b46d7c8cb1d6c
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-04-07 15:43:26 +0900

    Title: 구역 청소, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240407/구역 청소/district-cleaning.java
+++ 240407/구역 청소/district-cleaning.java
@@ -13,14 +13,14 @@ public class Main {
         if(b < c || d < a)
             len = (b-a) + (d-c);
         // 겹치는 경우
-        else if(a <= c && c <= b)
+        else if(a <= c && c <= b && b < d)
             len = d - a;
-        else if(c <= a && a <= d)
+        else if(c <= a && a <= d && d < b)
             len = b - c;
         // 한 직선이 다른 직선 내에 포함되는 경우
-        else if(a <= c && d <= b)
+        else if(a < c && d < b)
             len = b - a;
-        else if(c <= a && b <= d)
+        else if(c < a && b < d)
             len = d - c;
 
         System.out.print(len);
