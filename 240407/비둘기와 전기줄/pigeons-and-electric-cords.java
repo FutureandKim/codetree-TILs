e67a6fb213101ef993888135f40f903f621486d8
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-04-07 22:35:47 +0900

    Title: 비둘기와 전기줄, Time: 155ms, Memory: 10MB, Status: Passed - CodeTree

--- 240407/비둘기와 전기줄/pigeons-and-electric-cords.java
+++ 240407/비둘기와 전기줄/pigeons-and-electric-cords.java
@@ -4,15 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[][] arr = new int[n][2];
-        int[] dove = new int[11];
-        int[] lastLoc = new int[11];
+        int[] dove = new int[11]; // 비둘기의 현재 위치
+        int[] lastLoc = new int[11]; // 비둘기의 마지막 위치
         int cnt = 0;
-/*
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < 2; j++)
-                arr[i][j] = sc.nextInt();
-        }*/
 
         for(int i = 0; i < 11; i++){
             dove[i] = -1;
@@ -24,7 +18,8 @@ public class Main {
             int num = sc.nextInt();
             int loc = sc.nextInt();
             if(dove[num] != -1){
-                if(loc == lastLoc[num])
+                // 현재 위치와 마지막 위치가 같다면 제외
+                if(loc == lastLoc[num]) 
                     continue;
                 else cnt++;
             }
