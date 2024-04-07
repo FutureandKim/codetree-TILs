da956f268b59673d5799ea00529d11f894e52eef
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-04-07 22:05:34 +0900

    Title: 전부 겹치는 선분 2, Time: 153ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240407/전부 겹치는 선분 2/overlapping-line-segments-2.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] line = new int[n][2];
+        int[] arr = new int[101];
+        boolean isOverlap = false;
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < 2; j++)
+                line[i][j] = sc.nextInt();
+        }
+
+        for(int i = 0; i < n; i++){
+            int start = line[i][0];
+            int fin = line[i][1];
+            for(int j = start; j <= fin; j++)
+                arr[j]++;
+        }
+
+        for(int i = 0; i < n; i++){
+            int start = line[i][0];
+            int fin = line[i][1];
+            for(int j = start; j <= fin; j++){
+                arr[j]--;
+                for(int k = 0; k < 101; k++){
+                    if(arr[k] == n-1){
+                        isOverlap = true;
+                        break;
+                    }
+                }
+                arr[j]++;
+            }
+        }
+
+
+        System.out.print(isOverlap ? "Yes" : "No");
+    }
+}
