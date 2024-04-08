05113667c2d20aed44a3836809741cfc700f5aaa
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-04-08 14:22:37 +0900

    Title: 연속된 숫자 만들기 2, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

--- 240408/연속된 숫자 만들기 2/create-consecutive-numbers-2.java
+++ 240408/연속된 숫자 만들기 2/create-consecutive-numbers-2.java
@@ -10,10 +10,15 @@ public class Main {
         for(int i = 0; i < 3; i++)
             arr[i] = sc.nextInt();
 
+        Arrays.sort(arr);
+
+        // 1. 이동하지 않아도 되는 경우
         if(arr[0] + 1 == arr[1] && arr[1] + 1 == arr[2])
             cnt = 0;
+        // 2. (4, 6, 10) (1, 7, 9) 처럼 두 숫자가 2 차이나는 경우
         else if(arr[0] + 2 == arr[1] || arr[1] + 2 == arr[2])
             cnt = 1;
+        // 3. 그 외
         else
             cnt = 2;
 
