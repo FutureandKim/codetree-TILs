ca65b20fd6d6af44a0e1a8de9d86f0e86cb76938
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-04-08 14:21:07 +0900

    Title: 연속된 숫자 만들기 2, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

--- 240408/연속된 숫자 만들기 2/create-consecutive-numbers-2.java
+++ 240408/연속된 숫자 만들기 2/create-consecutive-numbers-2.java
@@ -12,6 +12,8 @@ public class Main {
 
         if(arr[0] + 1 == arr[1] && arr[1] + 1 == arr[2])
             cnt = 0;
+        else if(arr[0] + 2 == arr[1] || arr[1] + 2 == arr[2])
+            cnt = 1;
         else
             cnt = 2;
 
