ab6d5300eaccd2875a9a12cdd449c10ff8fe29d7
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-04-09 16:23:28 +0900

    Title: 독서실의 거리두기 2, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

--- 240409/독서실의 거리두기 2/study-cafe-keeping-distance-2.java
+++ 240409/독서실의 거리두기 2/study-cafe-keeping-distance-2.java
@@ -7,10 +7,22 @@ public class Main {
         String str = sc.next();
         int[] seat = new int[n];
         int len = 0, maxLen = 0;
+        int minLen = 10000, a = 0;
 
         for(int i = 0; i < n; i++)
             seat[i] = str.charAt(i);
 
+        for(int i = 0; i < n; i++){
+            if(seat[i] == '0')
+                continue;
+            else if(seat[i] == '1'){
+                for(int j = i+1; j < n; j++){
+                    if(seat[j] == '1')
+                        minLen = Math.min(j - i, minLen);
+                }
+            }
+        }
+        //System.out.println(minLen);
         
         for(int i = 0; i < n; i++){
             if(seat[i] == '1')
@@ -42,12 +54,13 @@ public class Main {
             }
             maxLen = Math.max(maxLen, len);
         }
-
+/*
         // 나란히 나열된 1이 있는 경우
         for(int i = 0; i < n-1; i++){
             if(seat[i] == '1' && seat[i+1] == '1')
                 maxLen = 1;
-        }
+        }*/
+        maxLen = Math.min(maxLen, minLen);
         System.out.print(maxLen);
     }
 }
