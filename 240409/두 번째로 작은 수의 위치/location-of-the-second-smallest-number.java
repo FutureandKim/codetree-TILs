e7985223383cda1460ad42d7b0693f6c417709fa
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-04-09 17:07:00 +0900

    Title: 두 번째로 작은 수의 위치, Time: 160ms, Memory: 10MB, Status: Passed - CodeTree

--- 240409/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.java
+++ 240409/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.java
@@ -7,8 +7,8 @@ public class Main {
         int n = sc.nextInt();
         int[] num = new int[n];
         int[] arr = new int[n];
-        int loc = 0;
-        int cntArr0 = 0, cntArr1 = 0;
+        int loc = 0, min2 = 0;
+        int cntMin2 = 0;
 
         for(int i = 0; i < n; i++){
             num[i] = sc.nextInt();
@@ -17,20 +17,31 @@ public class Main {
         
         Arrays.sort(arr);
 
+        // 두번째로 작은 수 찾기
         for(int i = 0; i < n; i++){
-            if(arr[1] == num[i]){
+            if(arr[i] > arr[0]){
+                min2 = arr[i];
+                break;
+            } 
+            else if(i == n-1 && arr[0] == arr[i])
+                min2 = arr[0];
+        }
+
+        for(int i = 0; i < n; i++){
+            if(min2 == num[i]){
                 loc = i+1;
                 break;
             }
         }
 
+        // 두번째로 작은수가 여러개라면 -1
         for(int i = 0; i < n; i++){
-            if(arr[0] == num[i])
-                cntArr0++;
+            if(min2 == num[i])
+                cntMin2++;
         }
-
-        if(cntArr0 > 1)
+        if(cntMin2 > 1)
             loc = -1;
+
         System.out.print(loc);
     }
 }
