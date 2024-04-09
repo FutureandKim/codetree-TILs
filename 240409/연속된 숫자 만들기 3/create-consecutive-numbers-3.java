d78b00cf26e78bed3d3f9bbb6ab0190680131698
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-04-09 15:46:45 +0900

    Title: 연속된 숫자 만들기 3, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 240409/연속된 숫자 만들기 3/create-consecutive-numbers-3.java
+++ 240409/연속된 숫자 만들기 3/create-consecutive-numbers-3.java
@@ -1,33 +1,40 @@
 import java.util.Scanner;
 import java.util.Arrays;
+import java.math.BigInteger;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] p = new int[3];
-        int cnt = 0;
+        BigInteger[] p = new BigInteger[3];
+        BigInteger cnt = BigInteger.valueOf(0);
 
-        for(int i = 0; i < 3; i++)
-            p[i] = sc.nextInt();
-
-        if(p[1]-p[0] >= p[2]-p[1]){
+        for(int i = 0; i < 3; i++){
+            p[i] = sc.nextBigInteger();
+        }
+/*
+        if(p[1].subtract(p[2]).compareTo(p[2].subtract(p[1])) >= 0){
             while(true){
-                if(p[0] + 1 == p[1] && p[1] + 1 == p[2])
+                if(p[0].add(BigInteger.ONE).equals(p[1]) && p[1].add(BigInteger.ONE).equals(p[2]))
                     break;
-                p[2] = p[1] - 1;
+                p[2] = p[1].subtract(BigInteger.ONE);
                 Arrays.sort(p);
-                cnt++;
+                cnt = cnt.add(BigInteger.valueOf(1));
             }
-        }      
+        }   
         else {
             while(true){
-                if(p[0] + 1 == p[1] && p[1] + 1 == p[2])
+                if(p[0].add(BigInteger.ONE).equals(p[1]) && p[1].add(BigInteger.ONE).equals(p[2]))
                     break;
-                p[0] = p[1] + 1;
+                p[0] = p[1].add(BigInteger.ONE);
                 Arrays.sort(p);
-                cnt++;
+                cnt = cnt.add(BigInteger.valueOf(1));
             }
         }  
+        */
+        if(p[1].subtract(p[0]).compareTo(p[2].subtract(p[1])) >= 0)
+            cnt = p[1].subtract(p[0]).subtract(BigInteger.ONE);
+        else
+            cnt = p[2].subtract(p[1]).subtract(BigInteger.ONE);
         System.out.print(cnt);
 
     }
