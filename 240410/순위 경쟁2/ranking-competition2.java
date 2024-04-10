305fa51d0e765a66dd4445e590aa9e6fb9eaef11
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-04-10 11:36:53 +0900

    Title: 순위 경쟁2, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240410/순위 경쟁2/ranking-competition2.java
+++ 240410/순위 경쟁2/ranking-competition2.java
@@ -5,8 +5,8 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int a = 0, b = 0, cnt = 0;
-        String s = "AB"; // 명예에 전당에 올라가있는 사람
-        String win = ""; // 명예에 전당에 새로 올라가는 사람
+        String origin = "AB"; // 명예에 전당에 올라가있는 사람
+        String new_winner = ""; // 명예에 전당에 새로 올라가는 사람
 
         for(int i = 0; i < n; i++){
             char c = sc.next().charAt(0);
@@ -18,15 +18,16 @@ public class Main {
                 b += score;
 
             if(a == b)
-                win  = "AB";
+                new_winner  = "AB";
             else if(a < b)
-                win = "B";
+                new_winner = "B";
             else if(a > b)
-                win = "A";
+                new_winner = "A";
 
-            if(!s.equals(win)){
+            // 명예의 전당에 있던 사람과 새롭게 올라간 사람이 다르다면 cnt 증가
+            if(!origin.equals(new_winner)){
                 cnt++;
-                s = win;
+                new_winner = origin;
             }
         }
         System.out.print(cnt);
