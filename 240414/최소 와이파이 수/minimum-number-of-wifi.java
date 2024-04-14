76e72d0d2acf730665a4781fe38addc8cddbc1b9
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-04-14 18:12:04 +0900

    Title: 최소 와이파이 수, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240414/최소 와이파이 수/minimum-number-of-wifi.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+        int wifi = 0, cntLeft = 0;
+
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        for(int i = 0; i < n; i++){
+            if(arr[i] == 1)
+                cntLeft++;
+            
+            if(cntLeft == m+1){
+                wifi++;
+                cntLeft = 0;
+            }
+        }
+        System.out.print(wifi);
+    }
+}
