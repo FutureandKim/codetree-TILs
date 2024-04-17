4c8773d6e5e7603a05dfebeff093c79780934637
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-04-17 16:58:11 +0900

    Title: 행복한 수열의 개수, Time: 282ms, Memory: 20MB, Status: Passed - CodeTree

--- 240417/행복한 수열의 개수/number-of-happy-sequence.java
+++ 240417/행복한 수열의 개수/number-of-happy-sequence.java
@@ -13,32 +13,36 @@ public class Main {
                 arr[i][j] = sc.nextInt();
         }
 
-        // 행에서 행복한 수열 찾기
-        for(int i = 0; i < n; i++){
-            int same = 1;
-            for(int j = 0; j < n-1; j ++){
-                if(arr[i][j] == arr[i][j+1])
-                    same += 1;
-                else
-                    same = 1;
-                if(same >= m){
-                    cnt++;
-                    break;
+        if(m == 1)
+            cnt = n*2;
+        else{
+            // 행에서 행복한 수열 찾기
+            for(int i = 0; i < n; i++){
+                int same = 1;
+                for(int j = 0; j < n-1; j ++){
+                    if(arr[i][j] == arr[i][j+1])
+                        same += 1;
+                    else
+                        same = 1;
+                    if(same >= m){
+                        cnt++;
+                        break;
+                    }
                 }
             }
-        }
 
-        // 열에서 행복한 수열 찾기
-        for(int j = 0; j < n; j++){
-            int same = 1;
-            for(int i = 0; i < n-1; i++){
-                if(arr[i][j] == arr[i+1][j])
-                    same += 1;
-                else
-                    same = 1;
-                if(same >= m){
-                    cnt++;
-                    break;
+            // 열에서 행복한 수열 찾기
+            for(int j = 0; j < n; j++){
+                int same = 1;
+                for(int i = 0; i < n-1; i++){
+                    if(arr[i][j] == arr[i+1][j])
+                        same += 1;
+                    else
+                        same = 1;
+                    if(same >= m){
+                        cnt++;
+                        break;
+                    }
                 }
             }
         }
