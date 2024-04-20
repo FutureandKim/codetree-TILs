18ca5160687b89629b9e9ce62061a8aeb786a2e2
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-04-20 14:10:28 +0900

    Title: 정수 명령 처리 5, Time: 450ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240420/정수 명령 처리 5/process-numeric-commands-5.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+    public static ArrayList<Integer> arr = new ArrayList<>();
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = sc.nextInt();
+
+        for(int i = 0; i < n; i++){
+            String str = sc.next();
+            commandProcessing(str);
+        }
+    }
+
+    public static void commandProcessing(String str){
+        if(str.equals("push_back")){
+                int num = sc.nextInt();
+                arr.add(num);
+        }
+        else if(str.equals("pop_back"))
+            arr.remove(arr.size()-1);
+        else if(str.equals("size"))
+            System.out.println(arr.size());
+        else if(str.equals("get")){
+            int num = sc.nextInt();
+            System.out.println(arr.get(num-1));
+        }
+    }
+}
