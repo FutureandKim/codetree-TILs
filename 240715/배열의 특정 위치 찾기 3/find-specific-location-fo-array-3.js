0c7e4a030de698ef12726e8bd6e3f6ce3dd31bc1
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-07-15 23:16:52 +0900

    Title: 배열의 특정 위치 찾기 3, Time: 168ms, Memory: 15MB, Status: Passed - CodeTree

--- 240715/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.js
+++ 240715/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.js
@@ -3,9 +3,10 @@ let input = fs.readFileSync(0).toString().trim().split(" ").map(Number);
 
 let sum = 0;
 for(let i = 0; i < input.length; i++){
-    sum += input[i];
-    if(input[i] == 0)
+    if(input[i] == 0){
+        sum += input[i-3] + input[i-2] + input[i-1];
         break;
+    }
 }
 
 console.log(sum);
