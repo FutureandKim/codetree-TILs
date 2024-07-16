3446fb79c7c7f6ad7e83b77318768df4dc7fc16f
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-07-16 21:43:21 +0900

    Title: 1-9 개수 세기, Time: 193ms, Memory: 47MB, Status: Passed - CodeTree

---
+++ 240716/1-9 개수 세기/count-one-to-nine.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+let [n, nums] = fs.readFileSync(0).toString().trim().split("\n");
+let arr = nums.split(" ").map(Number);
+
+//숫자별 출현 횟수 배열
+let cntArr = Array(9).fill(0);
+
+for(let i = 0; i < arr.length; i++){
+    cntArr[arr[i]-1]++;
+}
+
+for(let i = 0; i < 9; i++)
+    console.log(cntArr[i]);
