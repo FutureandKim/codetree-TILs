14a5195b37464c270602022e3d386aa059e26aa4
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-08-04 02:27:51 +0900

    Title: 되돌아오기, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

--- 240804/되돌아오기/come-back.java
+++ 240804/되돌아오기/come-back.java
@@ -6,24 +6,26 @@ public class Main {
         int n = sc.nextInt();
         int[] dot = new int[]{0, 0}; // 시작 위치
 
-        int cnt = 0;
-        int res = -1;
-        boolean isComeback = false;
+        int cnt = 0; // 움직인 거리
+        int res = -1; // 원점으로 돌아올 때까지 걸린 거리
+        boolean isReturn = false; // 원점으로 돌아온 적 있는지 판단
 
         for(int i = 0; i < n; i++){
             char dir = sc.next().charAt(0);
             int m = sc.nextInt();
+
             for(int j = 0; j < m; j++){
-                cnt ++;
-                if(move(dot, dir) && !isComeback){
+                cnt++;
+                if(move(dot, dir) && !isReturn){
                     res = cnt;
-                    isComeback = true;
+                    isReturn = true;
                 }
             }
         }
         System.out.print(res);
     }
 
+    // 움직임 동작 및 원점에 돌아왔는지 판단하는 함수
     public static boolean move(int[] dot, char dir){
         switch(dir){
             case 'N':
@@ -38,7 +40,6 @@ public class Main {
             case 'W':
                 dot[0] -= 1;
         }
-
         if(dot[0] == 0 && dot[1] == 0)
             return true;
         return false;
