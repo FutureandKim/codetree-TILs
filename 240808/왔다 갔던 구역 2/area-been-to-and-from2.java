ff90f8d95eeaab66b75e333b76b651afb122ce68
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-08-08 22:58:09 +0900

    Title: 왔다 갔던 구역 2, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

--- 240808/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 240808/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -4,32 +4,33 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] line = new int[1000];
-        int idx = 500;
+        int[] line = new int[4001];
+        int idx = 2000;
 
         for(int i = 0; i < n; i++){
             int x = sc.nextInt();
             char c = sc.next().charAt(0);
             if(c == 'R'){
                 for(int j = 0; j < x; j++){
-                    line[idx] += 1;
+                    line[idx]++;
                     idx++;
                 }
-            }
-            else if(c == 'L'){
+            } else if(c == 'L'){
                 for(int j = 0; j < x; j++){
-                    line[idx] += 1;
                     idx--;
+                    line[idx]++;
                 }
             }
         }
 
         int res = 0;
-        for(int i = 0; i < 1000; i++){
-            if(line[i] >= 2) {
+
+        for (int i = 0; i < 4001; i++) {
+            if (line[i] >= 2) {
                 res++;
             }
         }
+
         System.out.println(res);
     }
 }
