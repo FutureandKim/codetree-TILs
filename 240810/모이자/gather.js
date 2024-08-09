266c5dcf96022c9c58f50d0f706968f2dc1432de
Mirae Kim <95979743+FutureandKim@users.noreply.github.com>
2024-08-10 01:12:42 +0900

    Title: 모이자, Time: 178ms, Memory: 14MB, Status: Passed - CodeTree

--- 240810/모이자/gather.js
+++ 240810/모이자/gather.js
@@ -2,16 +2,14 @@ const fs = require("fs");
 const [n, h] = fs.readFileSync(0).toString().trim().split("\n");
 const house = h.split(" ").map(Number);
 
-let minDis = [];
-
+let minDis = Number.MAX_SAFE_INTEGER;
 
 for(let i = 0; i < Number(n); i++){
     let dis = 0;
     for(let j = 0; j < Number(n); j++){
         dis += house[j] * Math.abs(j - i);
     }
-    minDis.push(dis);
+    minDis = Math.min(minDis, dis);
 }
 
-minDis.sort();
-console.log(minDis[0]);
+console.log(minDis);
