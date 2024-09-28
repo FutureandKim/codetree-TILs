a7bef4ec5427fbe8f99e6af68f8b6d98b4b264e8
김미래 <95979743+FutureandKim@users.noreply.github.com>
2024-09-29 00:58:22 +0900

    Title: 가장 많은 데이터, Time: 1138ms, Memory: 40MB, Status: Passed - CodeTree

---
+++ 240929/가장 많은 데이터/most-frequent-data.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        HashMap<String, Integer> arrHash = new HashMap<>();
+        int n = sc.nextInt();
+
+        int max = Integer.MIN_VALUE;
+
+        for(int i = 0; i < n; i++){
+            String str = sc.next();
+            if(arrHash.containsKey(str)){
+                int strNum = arrHash.get(str);
+                arrHash.put(str, strNum+1);
+            } else {
+                arrHash.put(str, 1);
+            }
+
+            if(arrHash.get(str) > max)
+                max = arrHash.get(str);
+        }
+
+        System.out.print(max);
+    }
+}
