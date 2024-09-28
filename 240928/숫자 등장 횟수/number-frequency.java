9fbed4c6abd3a81efc8c22a8e14fb7fbb89610f9
김미래 <95979743+FutureandKim@users.noreply.github.com>
2024-09-28 23:30:03 +0900

    Title: 숫자 등장 횟수, Time: 1458ms, Memory: 52MB, Status: Wrong Answer - CodeTree

--- 240928/숫자 등장 횟수/number-frequency.java
+++ 240928/숫자 등장 횟수/number-frequency.java
@@ -12,13 +12,21 @@ public class Main {
 
         for(int i = 0; i < n; i++){
             arr[i] = sc.nextInt();
-            hashNum.put(arr[i], i+1);
+            if(hashNum.containsKey(arr[i])){
+                int numCnt = hashNum.get(arr[i]);
+                hashNum.put(arr[i], numCnt+1);
+            } else {
+                hashNum.put(arr[i], 1);
+            }
         }
 
         for(int i = 0; i < m; i++){
             int num = sc.nextInt();
-            int numCnt = hashNum.get(num) == null ? 0 : hashNum.get(num);
-            System.out.print(numCnt + " ");
+            if(hashNum.containsKey(num)){
+                System.out.print(hashNum.get(num) + " ");
+            } else {
+                System.out.print(0);
+            }
         }
     }
 }
