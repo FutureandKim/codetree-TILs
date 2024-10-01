c3c0e04ea02c549759ba0cefb004ced1c56a2e9e
김미래 <95979743+FutureandKim@users.noreply.github.com>
2024-10-01 13:40:20 +0900

    Title: 특별한 문자, Time: 118ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241001/특별한 문자/special-character.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int strLen = str.length();
+        HashMap<Character, Integer> charHash = new HashMap<>();
+
+        for(int i = 0; i < strLen; i++){
+            int cnt = charHash.getOrDefault(str.charAt(i), 0);
+            charHash.put(str.charAt(i), cnt + 1);
+        }
+
+        boolean foundChar = false;
+        for(char c : charHash.keySet()){
+            if(charHash.get(c) == 1){
+                System.out.print(c);
+                foundChar = true;
+                break;
+            }
+        }
+
+        if(!foundChar) {
+            System.out.print("None");
+        }
+    }
+}
