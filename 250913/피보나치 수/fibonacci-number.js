c39139a042079b4e7f12198875111941624eff3c
김미래 <95979743+FutureandKim@users.noreply.github.com>
2025-09-13 18:36:03 +0900

    Title: 피보나치 수, Time: 66ms, Memory: 10MB, Status: Passed - Codetree

--- 250913/피보나치 수/fibonacci-number.js
+++ 250913/피보나치 수/fibonacci-number.js
@@ -1,13 +1,18 @@
 const fs = require("fs");
-const input = fs.readFileSync(0).toString().trim().split('\n');
+const input = fs.readFileSync(0).toString().trim().split("\n");
 
 const n = Number(input[0]);
 
+const memo = [];
 const getFibo = (n) => {
-    if(n <= 2) {
-        return 1;
+    if(n <= 1){
+        return n;
     }
-    return getFibo(n-1) + getFibo(n-2);
+    if(memo[n] !== undefined) {
+        return memo[n]
+    }
+
+    memo[n] = getFibo(n-1) + getFibo(n-2);
+    return memo[n]
 }
 console.log(getFibo(n));
-
